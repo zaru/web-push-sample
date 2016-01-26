521f9a64e5d57b6c5fe131a97fd3f5ec83051a5d
zaru <sakuraba@basicinc.jp>
2016-01-26 11:40:00 +0900

    サンプル作成

---
+++ worker.js
@@ -0,0 +1,56 @@
+if (typeof window !== "undefined") {
+  document.addEventListener('DOMContentLoaded', function() {
+
+    // ServiceWorkerのインストール
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistration().then( function(r) {
+        navigator.serviceWorker.register("/worker.js", { scope: "/" })
+          .then( function(registration) {
+            console.log('インストール完了');
+          } )
+          .catch( function(error) {
+            console.log('インストールが正常にできませんでした');
+          }
+        );
+      });
+    }
+
+  });
+} else {
+  self.addEventListener("push", function() {
+    self.registration.showNotification("プッシュ通知だよ", {
+      body: "通知メッセージ( ᐛ👐)パァ",
+      icon: "https://pbs.twimg.com/profile_images/1303203427/zaru2png_400x400",
+      tag: "tag",
+      actions: [
+        {action: 'action1', title: "ボタンだよ😀"},
+        {action: 'action2', title: "こっちもボタン👻"}
+      ]
+    });
+  });
+
+  self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+
+    if (event.action === 'action1') {
+      clients.openWindow("/action1");
+    } else if (event.action === 'action2') {
+      clients.openWindow("/action2");
+    } else {
+      clients.openWindow("/");
+    }
+  }, false);
+}
+
+// プッシュ通知の許可ダイアログ＆endpointを取得
+function subscribe(registration) {
+  navigator.serviceWorker.ready.then(function(sw) {
+    Notification.requestPermission(function(permission) {
+      if(permission !== 'denied') {
+        sw.pushManager.subscribe({userVisibleOnly: true}).then(function(s) {
+          console.log(s.endpoint);
+        });
+      }
+    });
+  });
+}
